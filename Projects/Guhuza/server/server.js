8148a7d21d618471d616dad55b71c248b88ec802
Mohan Dai <mohanbhattarai706@gmail.com>
2025-11-08 02:23:40 -0500

    Update server.js

--- Projects/Guhuza/server/server.js
+++ Projects/Guhuza/server/server.js
@@ -9,8 +9,16 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS Configuration - ONLY THIS ONE
+app.use(cors({
+  origin: [
+    'https://bhattaraisudip.com.np',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
